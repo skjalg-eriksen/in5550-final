80142beba74152174a56d9ce3eb6656b222f009e
skjale <skjale@ifi.uio.no>
2019-05-16 11:05:34 +0200

    more features to plot.py

--- plot.py
+++ plot.py
@@ -5,13 +5,19 @@ import matplotlib.ticker as mtick
 import json
 import os
 import pandas
+from helpers import correctBoolean
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--paths', nargs='+', action='store')
+    parser.add_argument('--labels', nargs='+', action='store', default = None)
     parser.add_argument('--save', action='store', type=str, default = None)
+    parser.add_argument('--latex', action='store', default="false")
     args = parser.parse_args()
+    
+    args.tqdm = correctBoolean(args.latex, 'latex')
+    
     
     fig = plt.figure(1, figsize=(5,5))
     ax = plt.subplot()
@@ -21,28 +27,41 @@ def main():
 
     fig.suptitle('dev_accuracy')
     table = []
-    for path in args.paths:
-        print(path)
-        try:
-            with open('{}/epochs.JSON'.format(path)) as epochs:
-                data = [dict(json.loads(line)) for line in epochs]
-                accuracy = [ d['dev_accuracy'] for d in data]
-                f1 = [ d['Report']['macro avg']['f1-score'] for d in data]
-                best_iteration = np.argmax(accuracy)
-                table_elem = {'model': path.split('/')[2], 
-                              'dev_accuracy': accuracy[best_iteration],
-                              'macro F1-score': f1[best_iteration]}
-                table.append(table_elem)
-                plot, = plt.plot( accuracy, label=path.split('/')[2])
-        except: pass
+    for n, path in enumerate(args.paths):
+        with open('{}/epochs.JSON'.format(path)) as epochs:
+            # load json data
+            data = [dict(json.loads(line)) for line in epochs]
+            
+            # get accuracy and f1-score
+            accuracy = [ d['dev_accuracy'] for d in data]
+            f1 = [ d['Report']['macro avg']['f1-score'] for d in data]
+            
+            # get best epoch
+            best_iteration = np.argmax(accuracy)
+            table_elem = {'model': path.split('/')[2], 
+                          'dev_accuracy': accuracy[best_iteration],
+                          'macro F1-score': f1[best_iteration]}
+            table.append(table_elem)
+            # plot results
+            if args.labels is not None and n < len(args.labels):
+                plot, = plt.plot( accuracy, label=args.labels[n])
+            else:
+                plot, = plt.plot( accuracy, label=path)
+            
+
+            
     vals = ax.get_yticks()
     ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
     ax.legend()
     
     #table.sort(key=lambda x: x['macro F1-score'], reverse=True)
     df = pandas.DataFrame(table, columns=['model', 'dev_accuracy', 'macro F1-score'])
-    print(df.to_string(index=False))
-
+    
+    if (args.latex):
+        print(df.to_latex())
+    else:
+        print(df.to_string(index=False))
+        
     if args.save is not None:
         plt.savefig(args.save)
     plt.show()
